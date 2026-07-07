94ea2c1a648b594a9fe797a0a22be6e2d5db6849
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-07-07 12:08:13 +0500

    Add AI task: Gapni tahlil qilish

---
+++ ai_topshiriqlar/Gapni_tahlil_qilish.py
@@ -0,0 +1,10 @@
+# Gapni tahlil qilish
+# Kurs: Dasturlash / IT
+# Mavzu: f-string ⭐ — zamonaviy formatlash (f"{ism}")
+# Ball: 30
+# Aziz Academy — AI Topshiriq
+
+# Yechimingizni shu yerga yozing
+# Kirish: input(), chiqish: print()
+a = input()
+print(len(a.split()))
