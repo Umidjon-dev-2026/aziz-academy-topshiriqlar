6cf0954bd361493b1968c0666e81beac17fc40c1
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-07-24 19:29:42 +0500

    Add AI task: Harflarni sanash

---
+++ ai_topshiriqlar/Harflarni_sanash.py
@@ -0,0 +1,11 @@
+# Harflarni sanash
+# Kurs: Dasturlash / IT
+# Mavzu: String metodlari — 1: lower, upper, strip, title
+# Ball: 30
+# Aziz Academy — AI Topshiriq
+
+# Yechimingizni shu yerga yozing
+# Kirish: input(), chiqish: print()
+a = input()
+b = a.count("i")
+print(b)
