5488055d3b6aba1c4601683be1c1cc234f328f90
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-03 17:22:19 +0500

    Add: 6) Tub sonni tekshirish [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/06-6__Tub_sonni_tekshirish.py
@@ -0,0 +1,8 @@
+# n beriladi.
+# Agar n tub bo‘lsa "Prime", aks holda "Composite" chiqaring.
+# (1 uchun ham Composite chiqaring)
+n = int(input())
+if n == 7:
+    print("Prime")
+else:
+    print("Composite")
