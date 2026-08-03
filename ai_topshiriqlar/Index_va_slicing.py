5c0b1c70b7e019c5dae35afd9cb0102bfd029e45
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-03 17:38:55 +0500

    Add AI task: Index va slicing

---
+++ ai_topshiriqlar/Index_va_slicing.py
@@ -0,0 +1,12 @@
+# Index va slicing
+# Kurs: Dasturlash / IT
+# Mavzu: String metodlari — 1: lower, upper, strip, title
+# Ball: 50
+# Aziz Academy — AI Topshiriq
+
+# Yechimingizni shu yerga yozing
+# Kirish: input(), chiqish: print()
+a = input()
+print(a[:3])
+print(a[-3:])
+print(a[::-1])
