14ee22ea4e5e9a9f5dbd6a940796216f0c17eaba
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-05 16:55:30 +0500

    Add: 7) 1 dan n gacha tub sonlar [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/07-7__1_dan_n_gacha_tub_sonlar.py
@@ -0,0 +1,11 @@
+# n beriladi.
+# 1 dan n gacha bo‘lgan barcha tub sonlarni har qatorda bitta chiqar.
+n = int(input())
+for i in range(2, n + 1):
+    prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            prime = False
+            break
+    if prime:
+        print(i)
