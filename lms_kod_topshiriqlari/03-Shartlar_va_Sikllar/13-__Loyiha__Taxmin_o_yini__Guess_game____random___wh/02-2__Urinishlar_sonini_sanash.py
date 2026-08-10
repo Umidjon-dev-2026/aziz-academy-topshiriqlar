2bba46b13ffd3d2bf674ebf8a810b09b5b48a84c
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-10 17:31:43 +0500

    Add: 2) Urinishlar sonini sanash [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/02-2__Urinishlar_sonini_sanash.py
@@ -0,0 +1,11 @@
+# Yashirin son = 5
+# Foydalanuvchi to‘g‘ri topguncha kiritadi.
+# Oxirida nechta urinish ketganini chiqaring.
+yashirin_son = 5
+urinish = 0
+while True:
+    x = int(input())
+    urinish += 1
+    if x == yashirin_son:
+        break
+print(urinish)
