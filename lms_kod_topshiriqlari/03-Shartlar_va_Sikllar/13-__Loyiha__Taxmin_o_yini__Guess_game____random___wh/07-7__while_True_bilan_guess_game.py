f81a4bf3a00129a2a35a7ab058030e13f685a2a0
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-10 22:04:30 +0500

    Add: 7) while True bilan guess game [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/07-7__while_True_bilan_guess_game.py
@@ -0,0 +1,13 @@
+# Yashirin son = 9
+# while True ishlating.
+# To‘g‘ri topilganda break qiling.
+yashirin_son = 9
+while True:
+    x = int(input())
+    if x < yashirin_son:
+        print("Low")
+    elif x > yashirin_son:
+        print("High")
+    else:
+        print("Correct")
+        break
