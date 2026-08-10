7181a33c05886f25e8992677506dca54b2d6d540
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-10 21:42:36 +0500

    Add: 5) To‘g‘ri topilganda urinish bilan [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/05-5__To_g_ri_topilganda_urinish_bilan.py
@@ -0,0 +1,10 @@
+# Yashirin son = 4
+# Topilganda: "Correct in X tries" chiqaring.
+yashirin_son = 4
+urinish = 0
+while True:
+    x = int(input())
+    urinish += 1
+    if x == yashirin_son:
+        print(f"Correct in {urinish} tries")
+        break
