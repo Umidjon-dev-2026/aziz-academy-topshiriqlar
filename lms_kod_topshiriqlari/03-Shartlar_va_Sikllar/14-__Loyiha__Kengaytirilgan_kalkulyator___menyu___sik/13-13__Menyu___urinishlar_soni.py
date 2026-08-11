8b35b95e054deb41ac6ece945c7c3be23f52e12a
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-11 22:26:42 +0500

    Add: 13) Menyu + urinishlar soni [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/13-13__Menyu___urinishlar_soni.py
@@ -0,0 +1,14 @@
+# Har amal sanalsin.
+# Chiqishda nechta amal bajarilganini chiqaring.
+
+c = 0 
+while True:
+    line = input()
+    if line.strip() == "0":
+        break
+    parts = line.split()
+    a = int(parts[0])
+    b = int(parts[1])
+    tanlov = int(input())
+    c += 1
+print(c)
