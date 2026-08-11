b3f5d3a4968d955100ae900e2948a38a9a17c2a9
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-11 09:53:32 +0500

    Add: 14) Cheksiz urinish, lekin hisob bilan [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/14-14__Cheksiz_urinish__lekin_hisob_bilan.py
@@ -0,0 +1,11 @@
+# Yashirin son = 11
+# Cheksiz urinish.
+# Oxirida faqat urinishlar sonini chiqaring.
+yashirin_son = 11
+urinish = 0
+while True:
+    x = int(input())
+    urinish += 1
+    if x == yashirin_son:
+        print(urinish)
+        break
