6c3da6d75f219733cbd7c9f23ac30f2d012e250d
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-19 17:22:34 +0500

    Add: 9) Cheksiz loop + break shart [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/15-Infinite_loop_____cheksiz_sikl_muammosi_va_oldini_/09-9__Cheksiz_loop___break_shart.py
@@ -0,0 +1,10 @@
+# while True.
+# Son kiritiladi.
+# Agar manfiy bo‘lsa break.
+# Musbat bo‘lsa chiqarilsin.
+while True:
+    n = int(input())
+    if n < 0:
+        break
+    else:
+        print(n)
