3b08bc6013307b6a529294778094b20b2ed14c46
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-19 17:14:39 +0500

    Add: 6) Limit bilan cheksiz loop [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/15-Infinite_loop_____cheksiz_sikl_muammosi_va_oldini_/06-6__Limit_bilan_cheksiz_loop.py
@@ -0,0 +1,12 @@
+# n = int(input())
+# while True.
+# Sanagich ishlating.
+# n martadan keyin break qiling.
+
+n = int(input())
+a = 0
+while True:
+    a += 1
+    print(a)
+    if a == n:
+        break
