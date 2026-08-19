e41668d70dfc82d2c95f2e30cd2e0a56c757d83b
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-19 17:10:13 +0500

    Add: 5) Foydalanuvchi bilan to‘xtash [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/15-Infinite_loop_____cheksiz_sikl_muammosi_va_oldini_/05-5__Foydalanuvchi_bilan_to_xtash.py
@@ -0,0 +1,11 @@
+# while True:
+#     s = input()
+#     if s == 'exit': print('Bye'); break
+#     print(s)
+
+while True:
+    s = input()
+    if s == "exit":
+        print("Bye")
+        break
+    print(s)
