12cac503b5172ef3016a4fc0e6fda97dc8413471
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-19 21:57:39 +0500

    Add: 10) Birinchi va oxirgi yig‘indisi [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/01-List___yaratish_va_index_____ro_yxat_asoslari/10-10__Birinchi_va_oxirgi_yig_indisi.py
@@ -0,0 +1,8 @@
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Birinchi va oxirgi element yig'indisini chiqaring.
+n = int(input())
+lst = list(map(int, input().split()))
+a = lst[0]
+b = lst[-1]
+print(a + b)
