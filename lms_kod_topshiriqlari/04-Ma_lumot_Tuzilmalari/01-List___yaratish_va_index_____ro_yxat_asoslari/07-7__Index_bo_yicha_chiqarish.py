87188247438d548fb9a2ec288e7721361c9326d4
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-19 21:53:39 +0500

    Add: 7) Index bo‘yicha chiqarish [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/01-List___yaratish_va_index_____ro_yxat_asoslari/07-7__Index_bo_yicha_chiqarish.py
@@ -0,0 +1,7 @@
+# n = int(input())
+# lst = list(map(int, input().split()))
+# 0 va 2-indexdagi elementlarni chiqaring.
+n = int(input())
+list = list(map(int, input().split()))
+print(list[0])
+print(list[2])
