5dd5e625faca7cdf2704aad811ea8feb1ff2690b
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-19 21:47:36 +0500

    Add: 3) Oxirgi element [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/01-List___yaratish_va_index_____ro_yxat_asoslari/03-3__Oxirgi_element.py
@@ -0,0 +1,7 @@
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Oxirgi elementni manfiy index bilan chiqaring.
+n = int(input())
+lst = list(map(int, input().split()))
+x = lst[-1]
+print(x)
