34b6528a48f2d009dadf9cb3f98bb4c2c452b06a
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-21 17:03:43 +0500

    Add: 11) pop: index berilgan [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/02-List_metodlari___append__insert__remove__pop__sort/11-11__pop__index_berilgan.py
@@ -0,0 +1,8 @@
+# lst = [10, 20, 30, 40]
+# k beriladi.
+# lst.pop(k) ni chiqaring (olingan element).
+# Keyin listni chiqaring.
+list = [10, 20, 30, 40]
+x = int(input())
+print(list.pop(x))
+print(list)
