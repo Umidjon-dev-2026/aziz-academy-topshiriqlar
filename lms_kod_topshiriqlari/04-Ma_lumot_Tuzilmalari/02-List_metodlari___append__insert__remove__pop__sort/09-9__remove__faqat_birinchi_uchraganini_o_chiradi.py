acda5cfb2c89ffe9f01eb5e4ff05db5620e2c626
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-21 16:57:39 +0500

    Add: 9) remove: faqat birinchi uchraganini o‘chiradi [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/02-List_metodlari___append__insert__remove__pop__sort/09-9__remove__faqat_birinchi_uchraganini_o_chiradi.py
@@ -0,0 +1,9 @@
+# n = int(input())
+# lst = list(map(int, input().split()))
+# val = int(input())
+# remove(val) qiling va listni chiqaring.
+n = int(input())
+sonlar = list(map(int, input().split()))
+val = int(input())
+sonlar.remove(val)
+print(sonlar)
