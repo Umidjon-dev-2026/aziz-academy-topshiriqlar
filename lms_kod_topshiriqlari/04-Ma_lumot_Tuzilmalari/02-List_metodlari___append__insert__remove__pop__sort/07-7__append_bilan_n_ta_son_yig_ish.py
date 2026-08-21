933ecb27b44239e9ac7aa7e1afe0a69f213500f0
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-21 16:52:36 +0500

    Add: 7) append bilan n ta son yig‘ish [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/02-List_metodlari___append__insert__remove__pop__sort/07-7__append_bilan_n_ta_son_yig_ish.py
@@ -0,0 +1,9 @@
+# n va n ta son beriladi.
+# Bo‘sh list yarating va hammasini append bilan qo‘shing.
+# Oxirida listni chiqaring.
+n = int(input())
+sonlar = input().split()
+x = []
+for b in sonlar:
+    x.append(int(b))
+print(x)
