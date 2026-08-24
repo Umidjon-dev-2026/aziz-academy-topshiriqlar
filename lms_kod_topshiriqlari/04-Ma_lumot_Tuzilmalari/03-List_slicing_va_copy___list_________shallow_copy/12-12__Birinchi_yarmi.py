b1fdb51f08ce7b8c71a332c2625261dd5505c64d
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-24 21:52:29 +0500

    Add: 12) Birinchi yarmi [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/03-List_slicing_va_copy___list_________shallow_copy/12-12__Birinchi_yarmi.py
@@ -0,0 +1,6 @@
+# n va n ta son beriladi.
+# Listning birinchi yarmini slicing bilan chiqaring.
+# (Agar toq bo‘lsa, o‘rtadagi kirmasin)
+n = int(input())
+x = list(map(int, input().split()))
+print(x[:n // 2])
