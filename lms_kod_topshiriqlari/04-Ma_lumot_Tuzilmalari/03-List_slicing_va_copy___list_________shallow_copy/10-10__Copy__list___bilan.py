09c4d5bdf81c2b9b9cc4435296f5344acebe7e52
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-24 21:49:18 +0500

    Add: 10) Copy: list() bilan [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/03-List_slicing_va_copy___list_________shallow_copy/10-10__Copy__list___bilan.py
@@ -0,0 +1,13 @@
+# n = int(input())
+# a = list(map(int, input().split()))
+# val = int(input())
+# b = list(a)
+# b oxiriga val qo'shing.
+# a va b ni chiqaring.
+n = int(input())
+a = list(map(int, input().split()))
+val = int(input())
+b = list(a)
+b.append(val)
+print(a)
+print(b)
