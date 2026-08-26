70017a2c987211e13b5d6e98c4796d0469ce5479
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-08-26 16:49:37 +0500

    Add: 15) Yakuniy mashq: xavfsiz copy [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/03-List_slicing_va_copy___list_________shallow_copy/15-15__Yakuniy_mashq__xavfsiz_copy.py
@@ -0,0 +1,10 @@
+# n va n ta son beriladi.
+# Listni copy qiling.
+# Copy listni teskari qiling.
+# Avval original listni, keyin copy listni chiqaring.
+n = int(input())
+sonlar = list(map(int, input().split()))
+nusxa = sonlar[:]
+sonlar.reverse()
+print(nusxa)
+print(sonlar)
