03e957af8625f6013e3ae33cec9973f6773e75ad
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-09-03 21:12:04 +0500

    Add: 7) Tuple bo‘ylab for [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/05-Tuple___o_zgarmas_tuzilma__qachon_kerak/07-7__Tuple_bo_ylab_for.py
@@ -0,0 +1,7 @@
+# n = int(input())
+# t = tuple(map(int, input().split()))
+# Har bir elementni alohida qatorda chiqaring.
+n = int(input())
+t = tuple(map(int, input().split()))
+for x in t:
+    print(x)
