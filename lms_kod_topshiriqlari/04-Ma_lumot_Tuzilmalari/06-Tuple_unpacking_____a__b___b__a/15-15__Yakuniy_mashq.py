281fb11413d33eb5084fd33ba6bffc0dc7570909
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-09-04 17:31:38 +0500

    Add: 15) Yakuniy mashq [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/06-Tuple_unpacking_____a__b___b__a/15-15__Yakuniy_mashq.py
@@ -0,0 +1,10 @@
+# Uchta son beriladi.
+# Birinchi va oxirgisini alohida oling.
+# O‘rtadagilarni list qilib oling.
+# Hammasini chiqaring.
+n = int(input())
+sonlar = list(map(int, input().split()))
+a, *b, c = sonlar
+print(a)
+print(b)
+print(c)
