3779910c5a4a70773ad76452bd3985756af9e2b2
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-09-04 17:22:49 +0500

    Add: 8) O‘rtani ajratish [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/06-Tuple_unpacking_____a__b___b__a/08-8__O_rtani_ajratish.py
@@ -0,0 +1,10 @@
+# n = int(input())
+# lst = list(map(int, input().split()))
+# a, *b, c = lst
+# a, b, c ni chiqaring.
+n = int(input())
+lst = list(map(int, input().split()))
+a, *b, c = lst 
+print(a)
+print(b)
+print(c)
