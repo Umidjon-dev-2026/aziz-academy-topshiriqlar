fbc9f020a8e1fccdcac02436ce7bb53132293b7c
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-09-04 17:29:42 +0500

    Add: 14) Ko‘p qiymatni * bilan yig‘ish [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/06-Tuple_unpacking_____a__b___b__a/14-14__Ko_p_qiymatni___bilan_yig_ish.py
@@ -0,0 +1,9 @@
+# n va n ta son beriladi.
+# Birinchi sonni alohida oling.
+# Qolganlarini list qilib oling.
+# Ikkalasini chiqaring.
+n = int(input())
+x = list(map(int, input().split()))
+a, *b = x
+print(a)
+print(b)
