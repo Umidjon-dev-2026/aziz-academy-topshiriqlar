21e9a31230d9c13038227779482723065e717578
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-09-14 17:17:50 +0500

    Add: 8) Dublikatlarni yo‘qotish [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/10-Set___asoslar__yaratilish__in__len__takrorlanmasli/08-8__Dublikatlarni_yo_qotish.py
@@ -0,0 +1,6 @@
+# n = int(input())
+# nums = list(map(int, input().split()))
+# nechta unikal element borligini chiqaring
+n = int(input())
+nums = list(map(int, input().split()))
+print(len(set(nums)))
