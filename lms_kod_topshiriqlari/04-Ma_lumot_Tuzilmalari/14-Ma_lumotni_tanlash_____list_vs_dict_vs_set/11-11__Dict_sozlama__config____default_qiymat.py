c9d1055d67d27aeada0456f9643888c84cad3a4f
Umidjon-dev-2026 <komiljonovumidjon077@gmail.com>
2026-09-21 17:04:39 +0500

    Add: 11) Dict sozlama (config) — default qiymat [Umidjon-dev-2026]

---
+++ lms_kod_topshiriqlari/04-Ma_lumot_Tuzilmalari/14-Ma_lumotni_tanlash_____list_vs_dict_vs_set/11-11__Dict_sozlama__config____default_qiymat.py
@@ -0,0 +1,9 @@
+k = int(input())
+config = {}
+for _ in range(k):
+    key, value = input().split()
+    config[key] = int(value)
+q = int(input())
+for _ in range(q):
+    key = input()
+    print(config.get(key, 0))
